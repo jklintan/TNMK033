6d930362a27f6b6b78f4a57faebe2f67ec243d87
Josefine Klintberg <josefine.klintberg@hotmail.com>
2018-12-06 14:57:31 +0100

    First upload

---
+++ antalLegoSatser.php
@@ -0,0 +1,37 @@
+<html>
+    <script src="assets/js/main.js"></script>
+    <?php
+        //Counts all sets in the catalog and total amount of different types of pieces in the catalog
+
+        $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego"); //Connect to Lego database
+        if(mysqli_errno($connection)) {
+            die("<p>MySQL error:</p>\n<p>" . mysqli_error($connection) . "</p>\n</body>\n</html>\n"); //Error message if connection failed
+        }
+
+        //Number of unique sets
+        $setID = "SELECT COUNT(DISTINCT inventory.SetID) FROM inventory";
+        $setQuery = mysqli_query($connection, $setID);
+        $numbOfSets = mysqli_fetch_array($setQuery);
+        echo("Antal unika legosatser: ");
+        echo($numbOfSets[0]);
+
+        //Number of unique pieces
+        $legoID = "SELECT COUNT(DISTINCT inventory.ItemID) FROM inventory";
+        $legoQuery = mysqli_query($connection, $legoID);
+        $numbOfPieces = mysqli_fetch_array($legoQuery);
+        echo("Antal olika legobitar: ");
+        echo($numbOfPieces[0]);
+
+        //Store the results in an array
+        $totalsResult = array("$numbOfSets[0]", "$numbOfPieces[0]");
+        $totalsResultString = json_encode($totalsResult);
+
+        //Send the results with javascript
+        echo "<script type='text/javascript'>",
+        "createGraph('speedometer','$totalsResultString', '.statistics');",
+        "</script>"
+        ;
+
+        mysqli_close($connection);
+    ?>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Lego Project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/default.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
+    <script src="assets/js/main.js"></script>
+</head>
+<body>
+    <div class="start">
+        <nav>
+            <a href="/">Hem</a>
+            <a href="/faq">FAQ</a>
+        </nav>
+        <div class="centerBox">
+            <img src="assets/img/lego.svg" alt="">
+            <input type="text" name="" placeholder="Brick 2 x 2..." id="">
+            <p>Lorem ipsum dolor amet woke aesthetic hella kickstarter, craft beer vaporware sartorial air plant echo park drinking vinegar kale chips schlitz hashtag flannel. Blue bottle af jianbing trust fund migas venmo green juice man bun pabst meggings post-ironic etsy hexagon. Listicle pork belly seitan, cornhole man bun narwhal cray irony. Kinfolk fam readymade portland, truffaut authentic kitsch meditation asymmetrical biodiesel prism.</p>
+        </div>
+    </div>
+
+    <div class="stats">
+        <div class="grapher speedometer" data-grapher="{}"></div>
+    </div>
+</body>
+</html>
